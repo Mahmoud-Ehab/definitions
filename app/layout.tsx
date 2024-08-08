c31dcbc1ca7c11821acc0410fe79b1d29b13e8c0
Mahmoud-Ehab <mo.ehab150@gmail.com>
2024-08-08 21:38:55 +0300

    prettier installed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Definitions",
-  description: "A full-stack web application dictionary",
+  title: 'Definitions',
+  description: 'A full-stack web application dictionary',
 };
 
 export default function RootLayout({
@@ -16,7 +16,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="h-24 w-screen bg-primary">
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            Taxing Laughter: The Joke Tax Chronicles
+          </h1>
+        </header>
+        {children}
+        <footer></footer>
+      </body>
     </html>
   );
 }
