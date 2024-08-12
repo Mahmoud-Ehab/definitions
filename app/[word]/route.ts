c0ef7f85c6384f2416f389717dcf3d8fa7572347
Mahmoud-Ehab <mo.ehab150@gmail.com>
2024-08-12 21:46:41 +0300

    NextResponse used instead of Response. And POST endpoint removed.

--- app/[word]/route.ts
+++ app/[word]/route.ts
@@ -1,5 +1,6 @@
 import { db, DB_Word } from '@/lib/db';
 import { Word } from '@/lib/types';
+import { NextResponse } from 'next/server'
 
 export async function GET(req: Request) {
   try {
@@ -21,34 +22,8 @@ export async function GET(req: Request) {
     }
     (word as unknown as Word).definitions = def_list;
 
-    return Response.json({ word });
+    return NextResponse.json({ word });
   } catch (e) {
-    return Response.error();
-  }
-}
-
-export async function POST(req: Request) {
-  try {
-    const url = new URL(req.url);
-    const word_text = url.pathname.slice(1).toLowerCase();
-
-    const stateFile = db.get(word_text.slice(0, 2));
-    const word = stateFile.getWhere((word) => (word as Word).text == word_text) as Word;
-    if (word) {
-      throw Error(`The word '${word_text}' already exist.`);
-    }
-
-    const payload = await req.json();
-    stateFile.add({
-      text: payload.text,
-      definitions: payload.definitions ? payload.definitions : {},
-      V: 1,
-      IV: 0,
-    });
-
-    return Response.json({ success: true });
-  } catch (e) {
-    console.error(e);
-    return Response.error();
+    return NextResponse.json({ error: "word not found" }, { status: 404 });
   }
 }
