9372bf1d46d86a758be45beb52252d153dadc296
Mahmoud E. Abdelsalam <mo.ehab150@gmail.com>
2024-08-16 05:28:17 +0300

    auth with google has been implemented with auth.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,23 +4,26 @@ import './globals.css';
 
 import { inter } from '@/app/fonts';
 import { Button } from '@/components/ui/button';
-import { LogIn } from 'lucide-react';
+import { LogIn, LogOut } from 'lucide-react';
 
 import { ThemeProvider } from '@/components/theme-provider';
 import { ModeToggleBtn } from '@/components/ModeToggleBtn';
-
 import { Toaster } from '@/components/ui/toaster';
+
+import { login, logout } from '@/lib/auth-actions'
+import { auth } from '@/auth'
 
 export const metadata: Metadata = {
   title: 'Definitions',
   description: 'A full-stack web application dictionary',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth()
   return (
     <html lang="en">
       <body className={cn(inter.className, 'overflow-x-hidden antialiased')}>
@@ -32,10 +35,13 @@ export default function RootLayout({
         >
           <header className="flex h-16 w-screen items-center justify-between bg-background px-8 md:px-12">
             <ModeToggleBtn />
-            <Button variant="outline">
-              <LogIn className="mr-2" />
-              Login with Google
-            </Button>
+            {session?.user && <label className="text-sm">You have logged in as {session.user.name}</label>}
+            <form action={session?.user ? logout : login}>
+              <Button variant="outline" type="submit">
+                {session?.user ? <LogOut className="mr-2" /> : <LogIn className="mr-2" />}
+                {session?.user ? "Logout" : "Login with Google"}
+              </Button>
+            </form>
           </header>
           {children}
           <Toaster />
